d778d0e8eecdb30ae78fcbf8ed88ac19dfeaf324
perkinson1251 <tttrashutin@gmail.com>
2024-08-15 16:13:51 +0300

    feat: add shadcdn and tailwind

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,4 @@
+<script lang="ts" setup></script>
+<template>
+  <div>Dashboard page</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import Button from '@/components/ui/Button.vue';
+</script>
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <Button>Test button</Button>
+  </div>
+</template>
